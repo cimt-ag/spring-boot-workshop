a29dbd29dddbc2426f88be1914864fee58059622
Niklas Witzel <witzel.niklas@gmx.de>
2024-05-21 15:04:00 +0200

    BP1a: Unit and integration testing correction

--- src/test/java/de/cimtag/rateyourbooks/service/BookServiceTest.java
+++ src/test/java/de/cimtag/rateyourbooks/service/BookServiceTest.java
@@ -18,12 +18,14 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
   @Mock
