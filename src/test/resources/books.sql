7d75b73dfcd4606b0dacf2a4307ea928ed206872
Niklas Witzel <witzel.niklas@gmx.de>
2024-05-23 16:17:31 +0200

    BP8a: Adapted test properties and SQL script

--- src/test/resources/books.sql
+++ src/test/resources/books.sql
@@ -1,12 +1,14 @@
-INSERT INTO books (id, title, author)
+TRUNCATE rateyourbooks.books;
+
+INSERT INTO rateyourbooks.books (id, title, author)
 VALUES (1, 'Fourth Wing', 'Rebecca Yarros');
-INSERT INTO books (id, title, author)
+INSERT INTO rateyourbooks.books (id, title, author)
 VALUES (2, 'Im Grunde Gut', 'Rutger Bregman');
-INSERT INTO books (id, title, author)
+INSERT INTO rateyourbooks.books (id, title, author)
 VALUES (3, 'Utopien für Realisten', 'Rutger Bregman');
-INSERT INTO books (id, title, author)
+INSERT INTO rateyourbooks.books (id, title, author)
 VALUES (4, 'Eine kurze Geschichte der Menschheit', 'Yuval Noah Harari');
-INSERT INTO books (id, title, author)
+INSERT INTO rateyourbooks.books (id, title, author)
 VALUES (5, 'Baustellen der Nation', 'Philip Banse');
 
 ALTER TABLE books
