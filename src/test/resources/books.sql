1a7326918bb3e5521899031038ea9e77eb63ea13
Niklas Witzel <witzel.niklas@gmx.de>
2024-05-28 09:13:04 +0200

    BP11: Containerization

--- src/test/resources/books.sql
+++ src/test/resources/books.sql
@@ -1,14 +1,14 @@
-TRUNCATE rateyourbooks.books;
+TRUNCATE books;
 
-INSERT INTO rateyourbooks.books (id, title, author)
+INSERT INTO books (id, title, author)
 VALUES (1, 'Fourth Wing', 'Rebecca Yarros');
-INSERT INTO rateyourbooks.books (id, title, author)
+INSERT INTO books (id, title, author)
 VALUES (2, 'Im Grunde Gut', 'Rutger Bregman');
-INSERT INTO rateyourbooks.books (id, title, author)
+INSERT INTO books (id, title, author)
 VALUES (3, 'Utopien für Realisten', 'Rutger Bregman');
-INSERT INTO rateyourbooks.books (id, title, author)
+INSERT INTO books (id, title, author)
 VALUES (4, 'Eine kurze Geschichte der Menschheit', 'Yuval Noah Harari');
-INSERT INTO rateyourbooks.books (id, title, author)
+INSERT INTO books (id, title, author)
 VALUES (5, 'Baustellen der Nation', 'Philip Banse');
 
 ALTER TABLE books
